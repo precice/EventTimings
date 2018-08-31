85b97a721e42f2a3d308298b86dcb3dec8831358
Florian Lindner <florian.lindner@ipvs.uni-stuttgart.de>
2018-08-31 15:12:08 +0200

    Deactivate prefixes for stored events.

--- EventTimings.cpp
+++ EventTimings.cpp
@@ -289,10 +289,16 @@ void EventRegistry::put(Event* event)
 
 Event & EventRegistry::getStoredEvent(std::string const & name)
 {
+  // Reset the prefix for creation of a stored event. Using prefixes with stored events is possible
+  // but leads to unexpected results, such as not getting the event you want, because someone else up the
+  // stack set a prefix.
+  auto previousPrefix = prefix;
+  prefix = "";
   auto insertion = storedEvents.emplace(std::piecewise_construct,
                                         std::forward_as_tuple(name),
                                         std::forward_as_tuple(name, false, false));
 
+  prefix = previousPrefix;
   return std::get<0>(insertion)->second;
 }
 
